ec17c63200d2d2e09cb6c6d22e53537a56cbe4e5
Vakabunga <shesterov@gmail.com>
2020-04-11 21:40:47 +0300

    createRequest done

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,46 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  xhr = new XMLHttpRequest();
 
+  if (options.headers === undefined) {
+    for (var prop in options.headers) xhr.setRequestHeader(prop, options.headers[prop]);
+  }
+
+  xhr.responseType = options.responseType;
+  xhr.withCredentials = true;
+
+  if (options.method === 'GET') {
+    xhr.open(options.method, `${options.url}?mail=${options.data.mail}&password=${options.data.password}`);
+    xhr.send();
+  }
+
+  if (options.method === 'POST') {
+    formData = new FormData();
+    formData.append('mail', options.data.mail);
+    formData.append('password', options.data.password);
+    xhr.open(options.method, url);
+    xhr.send(formData);
+  }
+
+  function callback(err, response) {
+    if (err === null) {
+      console.log('Статус: ', err);
+      console.log('Данные ответа: ', response);
+    } else {
+      console.log('Ошибка: ', err);
+    }
+  }
+
+  if (xhr.status[0] === 2) {
+    let err = null;
+    let response = xhr.responseText;
+    callback(err, response);
+  }
+
+  if (xhr.status[0] === 4 || xhr.status === 5) {
+    let err = xhr.status;
+    let response = xhr.statusText;
+    callback(err, response);
+  }
 };
