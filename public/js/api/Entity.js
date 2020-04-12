1abadf7478615ab85e9da1833ce36c347e909d0e
Vakabunga <shesterov@gmail.com>
2020-04-12 14:46:45 +0300

    class Entity done

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -4,14 +4,18 @@
  * Имеет свойство HOST, равно 'https://bhj-diplom.letsdocode.ru'.
  * */
 class Entity {
+  constructor() {
+    this.url = '';
+    this.host = 'http://localhost:8000';
+  }
 
   /**
    * Запрашивает с сервера список данных.
    * Это могут быть счета или доходы/расходы
    * (в зависимости от того, что наследуется от Entity)
    * */
-  static list( data, callback = f => f ) {
-
+  static list(data, callback = (f) => f) {
+    createRequest({ url: `${this.host}${this.url}`, data, responseType: 'json', method: 'GET', callback });
   }
 
   /**
@@ -19,24 +23,26 @@ class Entity {
    * на сервер. (в зависимости от того,
    * что наследуется от Entity)
    * */
-  static create( data, callback = f => f ) {
-
+  static create(data, callback = (f) => f) {
+    data._method = 'PUT';
+    createRequest({ url: `${this.host}${this.url}`, data, responseType: 'json', method: 'POST', callback });
   }
-
   /**
    * Получает информацию о счёте или доходе/расходе
    * (в зависимости от того, что наследуется от Entity)
    * */
-  static get( id = '', data, callback = f => f ) {
-
+  static get(id = '', data, callback = (f) => f) {
+    data.id = id;
+    createRequest({ url: `${this.host}${this.url}`, data, responseType: 'json', method: 'GET', callback });
   }
 
   /**
    * Удаляет информацию о счёте или доходе/расходе
    * (в зависимости от того, что наследуется от Entity)
    * */
-  static remove( id = '', data, callback = f => f ) {
-
+  static remove(id = '', data, callback = (f) => f) {
+    data.id = id;
+    data._method = 'DELETE';
+    createRequest({ url: `${this.host}${this.url}`, data, responseType: 'json', method: 'POST', callback });
   }
 }
-
