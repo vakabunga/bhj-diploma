f029f8214dfb9986c926b75f214e75a03ad0acd3
Vakabunga <shesterov@gmail.com>
2020-04-12 15:04:26 +0300

    class Transaction done

--- public/js/api/Transaction.js
+++ public/js/api/Transaction.js
@@ -3,5 +3,9 @@
  * Управляет счетами пользователя.
  * Имеет свойство URL со значением '/transaction'
  * */
-class Transaction {
+class Transaction extends Entity {
+  constructor() {
+    super();
+    this.url = '/transaction';
+  }
 }
