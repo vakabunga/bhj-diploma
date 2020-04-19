f5eac5708a8ca8ecce194248965daf311b254b40
Vakabunga <shesterov@gmail.com>
2020-04-19 19:27:36 +0300

    call setRequestHeader to the right order; start to catch errors when open() and send(); start to return responses to the callback()

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,49 +1,80 @@
 /**
- * Основная функция для совершения запросов
- * на сервер.
+ * Основная функция для совершения запросов на сервер.
  * */
 const createRequest = (options = {}) => {
   let xhr = new XMLHttpRequest();
 
-  if (options.headers === undefined) {
-    for (var prop in options.headers) xhr.setRequestHeader(prop, options.headers[prop]);
-  }
-
-  options.responseType === undefined ? (xhr.responseType = 'json') : (xhr.responseType = options.responseType);
+  options.responseType === undefined
+    ? (xhr.responseType = 'json')
+    : (xhr.responseType = options.responseType);
   xhr.withCredentials = true;
 
   if (options.method === 'GET') {
-    xhr.open(options.method, `${options.url}?mail=${options.data.mail}&password=${options.data.password}`);
-    xhr.send();
+    const formData = null;
+    const requestUrl = `${options.url}?mail=${options.data.mail}&password=${options.data.password}`;
   }
 
   if (options.method === 'POST') {
-    formData = new FormData();
+    const formData = new FormData();
     formData.append('mail', options.data.mail);
     formData.append('password', options.data.password);
-    xhr.open(options.method, url);
-    xhr.send(formData);
+    const requestUrl = options.url;
   }
 
-  function callback(err, response) {
-    if (err === null) {
-      console.log('Статус: ', err);
-      console.log('Данные ответа: ', response);
-      return response;
+  try {
+    xhr.open(options.method, requestUrl);
+
+    if (options.headers != undefined) {
+      for (var prop in options.headers)
+        xhr.setRequestHeader(prop, options.headers[prop]);
     } else {
-      console.log('Ошибка: ', err);
+      xhr.setRequestHeader('Content-type', 'application/json');
     }
+
+    xhr.send(formData);
+  } catch (error) {
+    callback(error);
   }
+
+  // try {
+  //   if (options.method === 'GET') {
+  //     xhr.open(
+  //       options.method,
+  //       `${options.url}?mail=${options.data.mail}&password=${options.data.password}`
+  //     );
+  //     xhr.send();
+  //   }
+
+  //   if (options.method === 'POST') {
+  //     const formData = new FormData();
+  //     formData.append('mail', options.data.mail);
+  //     formData.append('password', options.data.password);
+  //     xhr.open(options.method, options.url);
+  //     xhr.send(formData);
+  //   }
+  // } catch (error) {
+  //   return (err = error);
+  // }
+
+  // function callback(err, response) {
+  //   if (err === null) {
+  //     // console.log('Статус: ', err);
+  //     // console.log('Данные ответа: ', response);
+  //     return response;
+  //   } else {
+  //     // console.log('Ошибка: ', err);
+  //     return err;
+  //   }
+  // }
 
   if (xhr.status[0] === 2) {
     let err = null;
-    let response = JSON.parse(xhr.responseText);
+    response = JSON.parse(xhr.responseText);
     callback(err, response);
   }
 
   if (xhr.status[0] === 4 || xhr.status === 5) {
-    let err = xhr.status;
-    let response = xhr.statusText;
-    callback(err, response);
+    let err = xhr.statusText;
+    callback(err);
   }
 };
